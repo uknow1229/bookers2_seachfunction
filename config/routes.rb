9bb74b5d440d5d7486f5842d9f6be4bb6b78e421
uknow1229 <86299044+uknow1229@users.noreply.github.com>
2023-04-18 04:38:37 +0000

    [Finish]bookers2_seachfunction

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
+  get "search" => "searches#search"
 
   resources :users, only: [:index,:show,:edit,:update] do
   resource :relationships, only: [:create, :destroy]
